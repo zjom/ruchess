4ee9a40b488b11e26f8e395b8102658140145ea0
Zihan Jin <admin@zihanjin.com>
2026-04-02 17:34:00 +1100

    feat: simple repl for debug

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
-fn main() {
-    let board = ruchess::Board::new();
-    println!("{}", board)
+use std::{error::Error, io};
+
+use ruchess::Square;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut board = ruchess::Board::new();
+
+    loop {
+        println!("{}", board);
+        let from: Square = get_input("From: ").parse()?;
+        let to: Square = get_input("To: ").parse()?;
+        board = board.move_(&from, &to);
+    }
+}
+
+fn get_input(prompt: &str) -> String {
+    println!("{}", prompt);
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    input.trim().to_string()
 }
