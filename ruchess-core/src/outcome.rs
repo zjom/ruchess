dc79881f11117c4f0d48e80e7fe87e639a79b63e
Zihan Jin <admin@zihanjin.com>
2026-04-04 13:33:01 +1100

    feat: include reason in Outcome

--- ruchess-core/src/outcome.rs
+++ ruchess-core/src/outcome.rs
@@ -2,6 +2,20 @@ use crate::Color;
 
 pub enum Outcome {
     Ongoing,
-    Win(Color),
-    Draw,
+    Win(Color, WinReason),
+    Draw(DrawReason),
+}
+
+pub enum WinReason {
+    Checkmate,
+    Resignation,
+    Timeout,
+}
+
+pub enum DrawReason {
+    Stalemate,
+    InsufficientMaterial,
+    FiftyMoveRule, // https://en.wikipedia.org/wiki/Fifty-move_rule
+    Repetition,
+    Agreement,
 }
