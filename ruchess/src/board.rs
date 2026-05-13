87c3b81359e229e192154a129d939b6ec1d4a197
Zihan Jin <admin@zihanjin.com>
2026-05-13 21:52:54 +1000

    fix: board print in reverse

--- ruchess/src/board.rs
+++ ruchess/src/board.rs
@@ -230,7 +230,7 @@ impl Default for Board {
 
 impl Display for Board {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for row in self.into_grid() {
+        for row in self.into_grid().iter().rev() {
             for c in row {
                 match c {
                     Some(p) => write!(f, "{}", p.as_char()),
