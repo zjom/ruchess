43b96a049d735d35243a8bef347deb0216815fb8
Zihan Jin <admin@zihanjin.com>
2026-05-13 17:42:29 +1000

    misc: move piece to its own file

---
+++ ruchess/src/piece.rs
@@ -0,0 +1,16 @@
+use crate::{color::Color, role::Role};
+#[derive(Debug, Clone, Copy)]
+pub struct Piece {
+    pub role: Role,
+    pub color: Color,
+}
+
+impl Piece {
+    pub fn as_char(&self) -> char {
+        let c = self.role.as_ascii();
+        match self.color {
+            Color::White => c,
+            Color::Black => c.to_ascii_uppercase(),
+        }
+    }
+}
