fd4d1fd93b04519e189fd5efdafde3c366e82215
Zihan Jin <admin@zihanjin.com>
2026-05-13 17:41:26 +1000

    misc: move piece to its own file

--- ruchess/src/lib.rs
+++ ruchess/src/lib.rs
@@ -11,24 +11,7 @@ pub mod role;
 
 pub mod color;
 
-pub mod piece {
-    use crate::{color::Color, role::Role};
-    #[derive(Debug, Clone, Copy)]
-    pub struct Piece {
-        pub role: Role,
-        pub color: Color,
-    }
-
-    impl Piece {
-        pub fn as_char(&self) -> char {
-            let c = self.role.as_ascii();
-            match self.color {
-                Color::White => c,
-                Color::Black => c.to_ascii_uppercase(),
-            }
-        }
-    }
-}
+pub mod piece;
 
 pub mod mve;
 pub mod position;
